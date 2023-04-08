f9cb951c7e5f22d13cdab705ff56ceaf6606ab1f
Burra <gezerbugra@gmail.com>
2023-04-08 04:04:44 +0300

    colors defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        violet: "#0A092D",
+        "pickled-bluewood": "#2E3856",
+        "royal-blue": "#423ED8",
+        "lightning-yellow": "#FFCD1F",
+        "athens-gray": "#EDEFF4",
+        "ebony-clay": "#282E3E",
+        comet: "#626A82",
       },
     },
   },
   plugins: [],
-}
+};
