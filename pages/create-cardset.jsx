27a7a106233001d7aa331523d19bb124ab5e018f
Burra <gezerbugra@gmail.com>
2023-04-13 01:45:08 +0300

    cardset creating page updated

--- pages/create-cardset.jsx
+++ pages/create-cardset.jsx
@@ -1,56 +1,92 @@
-import { useState, useRef, createRef } from "react";
+import { useState, useRef, createRef, useEffect } from "react";
 import Button from "@/components/button";
 import TextInput from "@/components/text_input";
 import CardInput from "@/components/card_input";
+import CardsetRepository from "@/data/repositories/cardset_repository";
 
 export default function CreateCardset() {
   const [cardInputs, setCardInputs] = useState([
-    <CardInput key={0} index={1} />,
+    { index: 1, ref: createRef() },
   ]);
-  const cardInputRefs = useRef([createRef()]);
+  const cardsetNameInputRef = useRef(null);
+  const [isAddCardButtonDisabled, setIsAddCardButtonDisabled] = useState(false);
 
   function submitCardset() {
-    const inputs = [];
-
-    cardInputRefs.current.forEach((ref) => {
-      const definitionInput = ref.current.querySelector(".definitionInput");
-      const termInput = ref.current.querySelector(".termInput");
-      inputs.push({
+    const cardsData = [];
+    let cardsetName = cardsetNameInputRef.current.querySelector("input").value;
+    cardInputs.forEach((input) => {
+      if (!input.ref.current) return; // check if ref exists
+      const definitionInput =
+        input.ref.current.querySelector(".definitionInput");
+      const termInput = input.ref.current.querySelector(".termInput");
+      cardsData.push({
         definition: definitionInput.value,
         term: termInput.value,
       });
     });
+    console.log(cardsData);
+    CardsetRepository.create(cardsetName, cardsData);
   }
 
   function handleAddCardInput() {
+    const lastCard = cardInputs[cardInputs.length - 1];
+    if (!lastCard.ref.current) return;
+
+    if (isAddCardButtonDisabled) return;
+
+    const newIndex = cardInputs.length + 1;
     setCardInputs((prevInputs) => [
       ...prevInputs,
-      <CardInput key={prevInputs.length} index={prevInputs.length + 1} />,
+      { index: newIndex, ref: createRef() },
     ]);
-    cardInputRefs.current.push(createRef());
+  }
+
+  function handleCardInput(index, inputs) {
+    setCardInputs((prevInputs) =>
+      prevInputs.map((input) =>
+        input.index === index ? { ...input, inputs } : input
+      )
+    );
+  }
+
+  function handleCardDelete(index) {
+    const remainingCards = cardInputs.filter((input) => input.index !== index);
+    // re-index all cards
+    const updatedCards = remainingCards.map((card, i) => ({
+      ...card,
+      index: i + 1,
+    }));
+    setCardInputs(updatedCards);
   }
 
   return (
     <div className="px-4 pt-6 md:px-12 lg:px-64">
       <div className="flex justify-between items-center lg:my-16">
         <h1 className="text-xl text-athens-gray font-bold">
-          Create new study set
+          Create newstudy set
         </h1>
-        <Button text="Create set" />
+        <Button text="Create set" onClick={submitCardset} />
       </div>
-      <div className="mt-4 lg:mt-12">
+      <div className="mt-4 lg:mt-12" ref={cardsetNameInputRef}>
         <TextInput placeholder="Enter title..." className="lg:w-2/5" />
       </div>
       <div className="mt-6">
-        {cardInputs.map((_, index) => (
-          <div key={index} ref={cardInputRefs.current[index]}>
-            <CardInput index={index + 1} />
+        {cardInputs.map((input) => (
+          <div key={input.index} ref={input.ref}>
+            <CardInput
+              index={input.index}
+              onInput={handleCardInput}
+              onDelete={handleCardDelete}
+            />
           </div>
         ))}
       </div>
       <div className="my-4 flex justify-center">
-        <button onClick={handleAddCardInput}>Yeni Ekle</button>
-        <button onClick={submitCardset}>Tüm Inputları Al</button>
+        <Button
+          text="Add card"
+          onClick={handleAddCardInput}
+          disabled={isAddCardButtonDisabled}
+        />
       </div>
     </div>
   );
