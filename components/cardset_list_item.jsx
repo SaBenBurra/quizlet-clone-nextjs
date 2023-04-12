3cb90ff03a1665c44428ca42180d83a447e5cc36
Burra <gezerbugra@gmail.com>
2023-04-12 15:11:10 +0300

    card component renamed to cardset list item

--- components/cardset_list_item.jsx
+++ components/cardset_list_item.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-export default function Card() {
+export default function CardsetListItem(props) {
   return (
     <Link href="#">
       <div className="group w-full bg-pickled-bluewood flex flex-col justify-between rounded-md h-1/5 md:h-1/6 pt-4 my-5 overflow-hidden">
         <div className="flex flex-col px-4">
           <h2 className="text-athens-gray font-bold text-lg md:text-2xl ">
-            card title
+            {props.title}
           </h2>
-          <h3 className="text-gull-gray font-bold">123 terms</h3>
+          <h3 className="text-gull-gray font-bold">{props.cardCount} terms</h3>
         </div>
         <div className="bg-white h-2 opacity-0 group-hover:opacity-100 duration-300"></div>
       </div>
