6304df685a79e0c3d54cfb19312d637bb031266a
Burra <gezerbugra@gmail.com>
2023-04-12 15:12:24 +0300

    cardsets listed in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import Card from "@/components/card";
+import CardsetListItem from "@/components/cardset_list_item";
+import CardsetRepository from "@/data/repositories/cardset_repository";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [cardsets, setCardsets] = useState([]);
+
+  useEffect(() => {
+    async function fetchCardsets() {
+      let cardsets = await CardsetRepository.getAll();
+      return cardsets;
+    }
+    fetchCardsets().then((cardsetList) => setCardsets(cardsetList));
+  }, []);
+
   return (
     <div className="h-screen p-6">
-      <Card title="card title" termCount="3" />
+      {cardsets.map((cardset) => (
+        <CardsetListItem
+          key={cardset.id}
+          title={cardset.name}
+          cardCount={cardset.cardCount}
+        />
+      ))}
     </div>
   );
 }
