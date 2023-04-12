db89f7c9a5183636c236c91fbc6adc10e565189f
Burra <gezerbugra@gmail.com>
2023-04-12 17:52:28 +0300

    cardset creating form updated

--- components/card_input.jsx
+++ components/card_input.jsx
@@ -1,11 +1,28 @@
 import TextInput from "./text_input";
+import { useRef } from "react";
 
-export default function CardInput() {
+export default function CardInput(props) {
+  const cardInputRef = useRef(null);
+
+  function handleInput() {
+    const definitionInput =
+      cardInputRef.current.querySelector(".definitionInput");
+    const termInput = cardInputRef.current.querySelector(".termInput");
+    const inputs = {
+      definition: definitionInput.value,
+      term: termInput.value,
+    };
+    console.log(inputs);
+  }
+
   return (
-    <div className="relative w-full bg-pickled-bluewood rounded-lg h-52 py-4 px-3 mb-4 lg:h-44">
+    <div
+      className="relative w-full bg-pickled-bluewood rounded-lg h-52 py-4 px-3 mb-4 lg:h-44"
+      ref={cardInputRef}
+    >
       <div className="flex justify-between items-center lg:h-1/5">
         <h2 className="absolute bottom-3 left-3 text-athens-gray lg:relative lg:bottom-0 lg:left-0">
-          1
+          {props.index}
         </h2>
         <span className="absolute bottom-3 right-3 lg:relative lg:bottom-0 lg:left-0 cursor-pointer">
           <svg
@@ -23,10 +40,10 @@ export default function CardInput() {
       <div className="lg:flex lg:h-full items-center justify-between">
         <TextInput
           placeholder="Enter definition..."
-          className="mb-8 lg:mb-0 lg:h-12"
+          className="definitionInput mb-8 lg:mb-0 lg:h-12"
         />
         <div className="hidden lg:block w-20"></div>
-        <TextInput placeholder="Enter term..." className="lg:h-12" />
+        <TextInput placeholder="Enter term..." className="termInput lg:h-12" />
       </div>
     </div>
   );
--- pages/create-cardset.jsx
+++ pages/create-cardset.jsx
@@ -1,8 +1,35 @@
+import { useState, useRef, createRef } from "react";
 import Button from "@/components/button";
-import CardInput from "@/components/card_input";
 import TextInput from "@/components/text_input";
+import CardInput from "@/components/card_input";
 
 export default function CreateCardset() {
+  const [cardInputs, setCardInputs] = useState([
+    <CardInput key={0} index={1} />,
+  ]);
+  const cardInputRefs = useRef([createRef()]);
+
+  function submitCardset() {
+    const inputs = [];
+
+    cardInputRefs.current.forEach((ref) => {
+      const definitionInput = ref.current.querySelector(".definitionInput");
+      const termInput = ref.current.querySelector(".termInput");
+      inputs.push({
+        definition: definitionInput.value,
+        term: termInput.value,
+      });
+    });
+  }
+
+  function handleAddCardInput() {
+    setCardInputs((prevInputs) => [
+      ...prevInputs,
+      <CardInput key={prevInputs.length} index={prevInputs.length + 1} />,
+    ]);
+    cardInputRefs.current.push(createRef());
+  }
+
   return (
     <div className="px-4 pt-6 md:px-12 lg:px-64">
       <div className="flex justify-between items-center lg:my-16">
@@ -15,12 +42,15 @@ export default function CreateCardset() {
         <TextInput placeholder="Enter title..." className="lg:w-2/5" />
       </div>
       <div className="mt-6">
-        <CardInput />
-        <CardInput />
-        <CardInput />
+        {cardInputs.map((_, index) => (
+          <div key={index} ref={cardInputRefs.current[index]}>
+            <CardInput index={index + 1} />
+          </div>
+        ))}
       </div>
       <div className="my-4 flex justify-center">
-        <Button text="Add new card" />
+        <button onClick={handleAddCardInput}>Yeni Ekle</button>
+        <button onClick={submitCardset}>Tüm Inputları Al</button>
       </div>
     </div>
   );
