2003432434844035de36d58f112a774676d87201
Burra <gezerbugra@gmail.com>
2023-04-13 01:43:35 +0300

    onClick prop added to Button component

--- components/button.jsx
+++ components/button.jsx
@@ -1,6 +1,14 @@
 export default function Button(props) {
   return (
-    <button className="bg-royal-blue hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+    <button
+      onClick={props.onClick}
+      className={
+        `bg-royal-blue ${
+          props.disabled === false && "hover:bg-blue-600"
+        } text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline` +
+        (props.disabled && " bg-gray-500 cursor-auto")
+      }
+    >
       {props.text}
     </button>
   );
