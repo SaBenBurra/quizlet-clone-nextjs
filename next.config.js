9fcdcf089c1dde51acdb22183b75b60084d58daf
Burra <gezerbugra@gmail.com>
2023-04-12 15:11:50 +0300

    API_URL added to .evn

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
